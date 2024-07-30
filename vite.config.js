94d304dda3705fd71b1a22f4a509f66d3195ee25
ZaHoussam <mrlhou62@gmail.com>
2024-07-30 19:01:54 +0100

    new: modify

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/koozina/",
-  build: {
-    outDir: "koozina",
-    assetsDir: "src",
-    manifest: true,
-  },
 });
